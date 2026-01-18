cfbcc05d9c30b783b37c3ee23f2f098a9dd8ab7c
Thivcoding <vantiv21@gmail.com>
2026-01-18 22:35:25 +0700

    change origins url

--- config/cors.php
+++ config/cors.php
@@ -19,13 +19,13 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'http://localhost:5173', 
+     'allowed_origins' => [
+        // React local (Vite)
+        'http://localhost:5173',
         'http://127.0.0.1:5173',
-        'https://project-ecommerce-laravel-reactjs-u.vercel.app/',
-        'http://localhost:8000',    
-        'http://127.0.0.1:8000',     
-        'https://project-ecommerce-laravel-api.onrender.com'
+
+        // React production (Vercel)
+        'https://project-ecommerce-laravel-reactjs-u.vercel.app',
     ],
 
     'allowed_origins_patterns' => [],
