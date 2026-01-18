d879ab6ddc35c03c786bc98369554a2a5778a127
Thivcoding <vantiv21@gmail.com>
2026-01-18 21:37:50 +0700

    update core

--- config/cors.php
+++ config/cors.php
@@ -22,6 +22,7 @@ return [
     'allowed_origins' => [
         'http://localhost:5173', 
         'http://127.0.0.1:5173',
+        'https://project-ecommerce-laravel-reactjs-u.vercel.app/',
         'http://localhost:8000',    
         'http://127.0.0.1:8000',     
         'https://project-ecommerce-laravel-api.onrender.com'
